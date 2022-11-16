0735d70ce7f99ee58bf498d05e5fb994e526a0c6
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2022-11-16 11:46:49 -0500

    update conecction

--- Main.java
+++ Main.java
@@ -2,10 +2,6 @@ class Main {
 	
 	public static void main(String[] args){
 	System.out.println("Hello world, desde Java + webhook");
-	System.out.println("Hello world, 2");
-	System.out.println("Hello world, 3");
-	System.out.println("Hello world, 4");
-	System.out.println("Hello world, 5");
-	System.out.println("Hello world, 6");
+	System.out.println("La conexion se ha realizado de forma correcta");
 	}
 }
