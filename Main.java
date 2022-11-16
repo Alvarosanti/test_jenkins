59dbd53ad60f0f752ad5169f13e59aee03eb38cd
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2022-11-16 11:09:28 -0500

    Update Main class 3

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main {
 	public static void main(String[] args){
 	System.out.println("Hello world, desde Java + webhook");
 	System.out.println("Hello world, 2");
+	System.out.println("Hello world, 3");
 	}
 }
