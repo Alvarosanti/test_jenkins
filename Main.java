29d727b3c69a43843225ab18c8a9db7504a476e5
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2022-11-16 13:18:29 -0500

    force error master

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
 class Main {
 	
 	public static void main(String[] args){
-	System.out.println("Hello world, desde Java + webhook");
-	System.out.println("Hello world, 2");
-	System.out.println("Hello world, 3");
-	System.out.println("Hello world, 4");
-	System.out.println("Hello world, 5");
-	System.out.println("Hello world, 6");
+	System.out.println("Hello world, desde Java + webhook")
+	System.out.println("La conexion se ha realizado de forma correcta")
+	System.out.println("Email test 3 2")
 	}
 }
